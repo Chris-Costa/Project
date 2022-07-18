ffe6d2d67f48375a3993c1d8032ed447d2cf3613
Chris-Costa <cdcosta807@gmail.com>
2022-07-18 17:04:33 -0400

    css home page and work to embed video links

--- cvFit/src/app/home/welcome.component.ts
+++ cvFit/src/app/home/welcome.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class WelcomeComponent {
   public pageTitle = 'Home';
-  public longTextChris = 'Need help getting started in your body building journey?  This is the place to be!  The team is inteligent, helpful, and personable.  They helped me to become the 5x Mr. Olymia champion that I am today';
-  public longTextArnold = 'Both fitness and intellectual political discorce can be found here.  Best fitness team in the region!';
-  public longTextSimone = 'I would not be where I am today without the help of the create personal training team.  They deserve some create for my olympic gold medals';
+  public longTextChris = 'Need help getting started in your body building journey?  This is the place to be!  The team is inteligent, helpful, and personable.  They helped me to become the person I am today.  I would highly recommend the in person training sessions as well as making use of all the awesome tools the team provides via the website';
+  public longTextArnold = 'Simply, with their help I achieved my goal of putting on weight, one which I have struggled with for years. With their help I have gained 13kg+ and my strength has more than doubled. They really helped me by giving me the tools to grow, such as diet and training regimes.';
+  public longTextSimone = 'Their Personal Training is really great. I was skinny but too unmotivated and shy to go to the gym. I had Tommy as my personal trainer and now I noticed a positive change in my appearance. I am happy and now more confident with myself. I definitely recommend Tommy and Susan to anyone looking for great quality personal training.';
 }
