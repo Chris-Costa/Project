1e739314178020e37b4f7cb3e5d02c87681e7ba9
Chris-Costa <cdcosta807@gmail.com>
2022-11-25 09:40:21 -0500

    generates permisions through microsoft

--- cvFit/src/app/app.module.ts
+++ cvFit/src/app/app.module.ts
@@ -59,7 +59,7 @@ const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigato
       auth: {
         clientId: '6063621e-ae81-4c3e-b0ee-6c9486c01725', // Application (client) ID from the app registration
         //authority: 'Enter_the_Cloud_Instance_Id_Here/55e374bf-374e-49de-a716-836ce6f714d1', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
-        authority: '55e374bf-374e-49de-a716-836ce6f714d1',
+        authority: 'https://login.microsoftonline.com/55e374bf-374e-49de-a716-836ce6f714d1',
         redirectUri: 'http://localhost:4200'// This is your redirect URI
       },
       cache: {
